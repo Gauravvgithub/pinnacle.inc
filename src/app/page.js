ae356fa184a79df9e36a2ed202c8da1b041de221
gauravvgithub <gauravraj7011@gmail.com>
2025-09-23 19:01:08 +0530

    preloader

--- src/app/page.js
+++ src/app/page.js
@@ -14,13 +14,13 @@ import ContactForm from "@/components/contactform/ContactForm";
 import MapOne from "@/components/map/MapOne";
 import FooterOne from "@/components/footer/FooterOne";
 import BackToTop from "@/components/BackToTop";
-import Preloader from "@/components/preloader/Preloader";
+// import Preloader from "@/components/preloader/Preloader";
 
 export default function Home() {
   return (
     <div className="#">
 
-      <Preloader />
+      {/* <Preloader /> */}
       <HeaderOne />
       <BannerOne />
       <AboutOne />
